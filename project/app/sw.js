aa3596307f47e46dc35a15039219ccab020cad86
Aleksandar Bozic <aleksandar.bozic@appculture.com>
2018-07-25 19:57:42 +0200

    Code lab, step 8.

--- project/app/sw.js
+++ project/app/sw.js
@@ -15,6 +15,32 @@ limitations under the License.
 */
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js');
 
+//const bgSyncPlugin = new workbox.backgroundSync.Plugin('dashboardr-queue');
+const showNotification = () => {
+  self.registration.showNotification('Background sync success!', {
+    body: '🎉`🎉`🎉`'
+  });
+};
+const bgSyncPlugin = new workbox.backgroundSync.Plugin(
+  'dashboardr-queue',
+  {
+    callbacks: {
+      queueDidReplay: showNotification
+      // other types of callbacks could go here
+    }
+  }
+);
+
+const networkWithBackgroundSync = new workbox.strategies.NetworkOnly({
+  plugins: [bgSyncPlugin],
+});
+
+workbox.routing.registerRoute(
+  /\/api\/add/,
+  networkWithBackgroundSync,
+  'POST'
+);
+
 if (workbox) {
   console.log(`Yay! Workbox is loaded 🎉`);
   workbox.precaching.precacheAndRoute([]);
